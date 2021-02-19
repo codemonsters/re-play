1f1bdfc86987f904c3b9cc5630aafdf28f372f1f
claudiam21 <claudiamasini21062001@gmail.com>
2021-02-19 19:32:39 +0100

    Añade boceto interfaz usuario partida de Blockyº

--- src/minigames/blocky/blocky.gd
+++ src/minigames/blocky/blocky.gd
@@ -1,13 +1,12 @@
 extends CanvasLayer
 
 func _ready():
-	$Screen/Menu.connect("blocky_start_game", self, "on_start_game")
+	var _ignored = $Screen/Menu.connect("blocky_start_game", self, "on_start_game")
 
 func on_start_game():
 	print("Empecemos")
-	var game_scene = load("game.tscn")
+	var game_scene = preload("res://minigames/blocky/game.tscn")
 
-
-	#get_node("Screen").remove_child(get_node("Menu"))
-	#get_node("Screen").queue_free()
-	get_parent().get_node("Blocky/Screen").add_child(game_scene)
+	get_node("Screen/Menu").queue_free()
+	var screen_node = get_parent().get_node("Blocky/Screen")
+	screen_node.add_child(game_scene.instance())
