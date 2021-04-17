1c42908a9bff08e1e6a2edf6b0712d758133ec86
Alejandro Sexto <a.sexto@gmail.com>
2021-04-17 19:41:04 +0200

    Cambios menores

--- src/minigames/blocky/tetrominos/tetromino.gd
+++ src/minigames/blocky/tetrominos/tetromino.gd
@@ -2,38 +2,16 @@ extends Node2D
 
 var sourcePosition
 var destinationPosition
+var dragging = false # flag set to true while dragging the tetromino
 
 
 func _ready():
 	connect("input_event", self, "_on_input_event")
 
-
-func _input(event):
-	#print("event")
-	pass
-
-func get_size():
-	var x_max = 0
-	var x_min = 0
-	var y_max = 0
-	var y_min = 0
+#func _enter_tree():
+#	self.connect("input_event", self, "_on_input_event")
 	
-	for child in get_children():
-		if child.position.x < x_min:
-			x_min = child.position.x
-		if child.position.x > x_max:
-			x_max = child.position.x
-		if child.position.y < y_min:
-			y_min = child.position.y
-		if child.position.y > y_max:
-			y_max = child.position.y
-	
-	var width = (x_max - x_min) - $Block1.get_size().x
-	var height = (y_max - y_min) - $Block1.get_size().y
-	
-	return Vector2(width, height)
-	
-
+		
 func _process(delta):
 	var shift_vector = (destinationPosition - position).normalized() * delta * 800
 	if shift_vector.length() > (destinationPosition - position).length():
@@ -42,9 +20,22 @@ func _process(delta):
 		position += shift_vector
 
 
-func _on_input_event():
-	print("TOCADO: ")
+
+func _on_input_event(viewport, event, shape_idx):
+	print("BOOM!!!!")
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			dragging = event.pressed
 
 
-func _on_Tetromino_input_event(viewport, event, shape_idx):
-	print("tocado2")
+#func _input(event):
+#	print("BANG!!!")
+#	if event is InputEventMouseButton:
+#		if event.button_index == BUTTON_LEFT and not event.pressed:
+#			dragging = false
+
+
+func _physics_process(dt):
+	# If dragging is enabled, use mouse position to calculate movement
+	if dragging:
+		position = get_global_mouse_position()
