164f01e5f029eca84c40f1766428166b3a8b6c22
Alejandro Sexto <a.sexto@gmail.com>
2021-04-24 13:58:25 +0200

    Actualizados nombres de funciones callback de los tetrominos

--- src/minigames/blocky/tetrominos/tetromino.gd
+++ src/minigames/blocky/tetrominos/tetromino.gd
@@ -3,9 +3,17 @@ extends Node2D
 var sourcePosition
 var destinationPosition
 var dragging = false # flag set to true while dragging the tetromino
-
+var mouse_inside = false # flag, values is true when the mouse is inside BoundingBox
 
 func _process(delta):
+	# TODO: Inmplementar drag and drop cuando el jugador hace clic y mouse_inside es true
+	if Input.is_action_just_released("click"):
+		dragging = false
+		print("NOT DRAGGING")
+	elif Input.is_action_pressed("click") and mouse_inside:
+		dragging = true
+		print("DRAGGING")
+
 	var shift_vector = (destinationPosition - position).normalized() * delta * 800
 	if shift_vector.length() > (destinationPosition - position).length():
 		position = destinationPosition
@@ -19,5 +27,9 @@ func _physics_process(_dt):
 		position = get_global_mouse_position()
 
 
-func _on_ColorRect_mouse_entered():
-	print("AHHHHHHHHHHHH") 
+func _on_BoundingBox_mouse_entered():
+	mouse_inside = true
+	
+
+func _on_BoundingBox_mouse_exited():
+	mouse_inside = false
