6f5d891dab7b2d6bc1812341e75a2122b9502da1
Braisa <oterolemabrais@gmail.com>
2021-05-07 20:03:37 +0200

    Luces 1

--- 3d/Camera.gd
+++ 3d/Camera.gd
@@ -6,6 +6,9 @@ func _ready():
 	follow = get_parent().get_node("KinematicBody")
 
 func _process(delta):
-	global_transform.origin.x = follow.global_transform.origin.x
-	global_transform.origin.y = follow.global_transform.origin.y + 2.5 + sin(follow.alpha) * 5
-	global_transform.origin.z = follow.global_transform.origin.z + cos(follow.alpha) * 5
+	var vector = Vector3()
+	vector.z = cos(follow.alpha) * 5
+	vector.y = sin(follow.alpha) * 5
+#	global_transform.origin.x = follow.global_transform.origin.x
+#	global_transform.origin.y = follow.global_transform.origin.y
+#	global_transform.origin.z = follow.global_transform.origin.z
